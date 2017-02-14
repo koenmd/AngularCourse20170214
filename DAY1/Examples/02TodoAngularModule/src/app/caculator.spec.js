44d7d1cebc3f2e3826270bc39bbb61de586e9f0c
Matthias Hutsebaut <Matthias Hutsebaut>
2017-02-14 14:20:48 +0100

    ex2

---
+++ DAY1/Examples/02TodoAngularModule/src/app/caculator.spec.js
@@ -0,0 +1,9 @@
+"use strict";
+var calculator_1 = require("./calculator");
+describe("calculator", function () {
+    it("should calculate the sum of two numbers", function () {
+        var calc = new calculator_1.Calculator();
+        var result = calc.calculateSum(5, 6);
+        expect(result).toBe(11);
+    });
+});
---
+++ DAY1/Examples/02TodoAngularModule/src/main.ts
@@ -0,0 +1,6 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/appModule/appModule';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+alert("module is geladen2");
